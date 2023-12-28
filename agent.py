d0933aa10dc048d1c60ea72514fdbb5599d6efa4
sidlakkoju <siddharth.lakkoju@gmail.com>
2023-12-28 15:51:29 -0500

    Added Model Testing code

--- agent.py
+++ agent.py
@@ -69,6 +69,9 @@ class agent:
     def save_model(self, path):
         torch.save(self.model.state_dict(), path)
     
+    def load_model(self, path):
+        self.model.load_state_dict(torch.load(path))
+    
 
     def replay(self):
         mini_batch = random.sample(self.memory, self.training_batch_size)
